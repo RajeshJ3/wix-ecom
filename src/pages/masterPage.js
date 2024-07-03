6c25778eecd033634f6a1a53784935c4630e8ed3
Rajesh Joshi <rajesh@Rajeshs-MacBook-Pro.local>
2024-07-03 08:08:03 +0530

    Added base code

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -1,10 +1,35 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+function handleClickPlayBtn() {
+  const playButton = $w("#play-btn");
+  playButton.hide();
+
+  const gameBox = $w("#game-box");
+  gameBox.show();
+}
+
+function handleCloseGame() {
+  const playButton = $w("#play-btn");
+  playButton.show();
+
+  const gameBox = $w("#game-box");
+  gameBox.hide();
+
+  const previewContainer = $w("#preview-container");
+  previewContainer.show();
+
+  const gameContainer = $w("#game-container");
+  gameContainer.hide();
+}
+
+function handleLoadGame() {
+  const previewContainer = $w("#preview-container");
+  previewContainer.hide();
+
+  const gameContainer = $w("#game-container");
+  gameContainer.show();
+}
 
 $w.onReady(function () {
-    // Write your JavaScript here
-
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
+  $w("#play-btn").onClick(handleClickPlayBtn);
+  $w("#close-game-btn").onClick(handleCloseGame);
+  $w("#load-game-btn").onClick(handleLoadGame);
 });
