ba744546497f0861a80471d9d05bd7a5365e4c96
Rajesh Joshi <joshirajesh448@gmail.com>
2024-07-03 10:21:43 +0530

    Generating Options

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -20,12 +20,54 @@ function handleCloseGame() {
   gameContainer.hide();
 }
 
+function generateRandomNumber() {
+  // generated a random number between 1 and 100
+
+  let decimals = 100;
+
+  // inital values
+  let found = false;
+  let randomNo = 0;
+
+  // looping till a valid number is found
+  while (!found) {
+    randomNo = Math.floor(Math.random() * decimals + 1);
+    if (randomNo > 0 && randomNo < 100) {
+      found = true;
+    }
+  }
+  return randomNo;
+}
+
+function putOption(key, value) {
+  $w(`#button-${key}`).label = value
+  console.log(key, value);
+}
+
+function generateOptions() {
+  let randomOptions = [];
+  for (let i = 0; i < Array(16).length; i++) {
+    randomOptions = [...randomOptions, generateRandomNumber()];
+  }
+  return randomOptions;
+}
+
+function generateGame() {
+  const options = generateOptions();
+  for (let i = 0; i < options.length; i++) {
+    const element = options[i];
+    putOption(i + 1, element);
+  }
+}
+
 function handleLoadGame() {
   const previewContainer = $w("#preview-container");
   previewContainer.hide();
 
   const gameContainer = $w("#game-container");
   gameContainer.show();
+
+  generateGame();
 }
 
 $w.onReady(function () {
